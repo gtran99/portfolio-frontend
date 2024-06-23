c873e8d95e89c5c170e09e0af0a975ebdf64fa01
gtran99 <gavintran1@gmail.com>
2024-06-23 13:45:56 -0700

    Initial commit

---
+++ src/Portfolio.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+const Portfolio = () => {
+    const [portfolio, setPortfolio] = useState(null);
+
+    useEffect(() => {
+        fetch('https://portfolio-backend-9tue.onrender.com')
+            .then(response => response.json())
+            .then(data => setPortfolio(data));
+    }, []);
+
+    if (!portfolio) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>{portfolio.name}</h1>
+            <p>{portfolio.bio}</p>
+            <h2>Projects</h2>
+            <ul>
+                {portfolio.projects.map((project, index) => (
+                    <li key={index}>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <a href={project.link}>Project Link</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Portfolio;
